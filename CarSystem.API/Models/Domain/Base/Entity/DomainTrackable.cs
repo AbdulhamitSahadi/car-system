6d34760d60fe1cb36370496318cecb429a8498bc
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 22:01:25 +0300

    UpdateToGenericType

--- CarSystem.API/Models/Domain/Base/Entity/DomainTrackable.cs
+++ CarSystem.API/Models/Domain/Base/Entity/DomainTrackable.cs
@@ -3,11 +3,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CarSystem.API.Models.Domain.Base.Entity
 {
-    public class DomainTrackable :  IDomainTrackable
+    public class DomainTrackable<TEntity> :  IDomainTrackable<TEntity>
     {
         [Key]
         public int Id { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public DateTime? CreatedAt { get; set; }
+
+        public ICollection<TEntity>? Admins_Entities { get; set; }
     }
 }
--- CarSystem.API/Models/Domain/Base/IEntity/IDomainTrackable.cs
+++ CarSystem.API/Models/Domain/Base/IEntity/IDomainTrackable.cs
@@ -1,9 +1,11 @@
 ﻿namespace CarSystem.API.Models.Domain.Base.IEntity
 {
-    public interface IDomainTrackable
+    public interface IDomainTrackable<TEntity>
     {
         int Id { get; set; }
         DateTime? UpdatedAt { get; set; }
         DateTime? CreatedAt { get; set; }
+
+        ICollection<TEntity>? Admins_Entities { get; set; }
     }
 }
