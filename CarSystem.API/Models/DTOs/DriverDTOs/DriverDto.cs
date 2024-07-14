6e4498d2cc03dd163ecb33262204daa5b6220042
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:28:16 +0300

    DriverDtoAdded

---
+++ CarSystem.API/Models/DTOs/DriverDTOs/DriverDto.cs
@@ -0,0 +1,13 @@
+﻿using CarSystem.API.Models.Domain;
+
+namespace CarSystem.API.Models.DTOs.DriverDTOs
+{
+    public class DriverDto
+    {
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public User User { get; set; }
+
+        public ICollection<License> OwnedLicenses { get; set; }
+    }
+}
