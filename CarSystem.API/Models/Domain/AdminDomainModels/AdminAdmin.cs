8509b8fbd76f5731e77b6a2faa4efbbb366b2932
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 21:59:17 +0300

    AdminAdmin_AdminDomainModelAdded

---
+++ CarSystem.API/Models/Domain/AdminDomainModels/AdminAdmin.cs
@@ -0,0 +1,10 @@
+﻿using CarSystem.API.Models.Domain.AdminDomainModels.Base.Entity;
+using CarSystem.API.Models.Domain.AdminDomainModels.Base.IEntity;
+
+namespace CarSystem.API.Models.Domain.AdminDomainModels
+{
+    public class AdminAdmin : AdminTrackable<Admin, int>
+    {
+
+    }
+}
