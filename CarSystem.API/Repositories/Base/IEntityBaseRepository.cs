3b089b5a5503741e8d6e8bb2c67f73d11d58e69f
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:02:07 +0300

    IEntityBaseRepositoryAdded

---
+++ CarSystem.API/Repositories/Base/IEntityBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace CarSystem.API.Repositories.Base
+{
+    public interface IEntityBaseRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, string? includeProperties = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate = null, string? includeProperties = null, bool tracked = true);
+        Task<bool> CreateAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(T entity);
+        Task<bool> IsExistAsync(Expression<Func<T, bool>> predicate = null);
+    }
+}
