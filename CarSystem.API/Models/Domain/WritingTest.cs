f0de09db286a148d566272bcd5f9763d9e0b85ab
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 13:49:18 +0300

    WritingTestDomainModelAdded

---
+++ CarSystem.API/Models/Domain/WritingTest.cs
@@ -0,0 +1,44 @@
+﻿using CarSystem.API.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarSystem.API.Models.Domain
+{
+    public class WritingTest
+    {
+        [Key]
+        public int Id { get; set; }
+
+        //[Required]
+        //[ForeignKey(nameof(Application))]
+        //public int ApplicationId { get; set; }
+        //public Application Application { get; set; }
+
+        [DisplayFormat(DataFormatString = "0:C2", ApplyFormatInEditMode = true)]
+        public decimal Score { get; set; }
+
+        [StringLength(500)]
+        public string? Notes { get; set; }
+
+        [Required]
+        public bool IsSuccess { get; set; }
+
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        [Required]
+        [ForeignKey("EditedBy")]
+        public int AdminId { get; set; }
+        public Admin EditedBy { get; set; }
+
+
+        [Required]
+        [ForeignKey("Test")]
+        public int TestId { get; set; }
+        public Test Test { get; set; }
+
+        //public ICollection<Test> Tests { get; set; }
+    }
+}
