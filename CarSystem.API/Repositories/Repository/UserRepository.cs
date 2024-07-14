21a4e226a655ec578abbd2378c79d5802dac1e2a
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:20:28 +0300

    UserRepositoryAdded

---
+++ CarSystem.API/Repositories/Repository/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using CarSystem.API.Data;
+using CarSystem.API.Models.Domain;
+using CarSystem.API.Repositories.Base;
+using CarSystem.API.Repositories.IRepositories;
+
+namespace CarSystem.API.Repositories.Repository
+{
+    public class UserRepository : EntityBaseRepository<User> , IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
