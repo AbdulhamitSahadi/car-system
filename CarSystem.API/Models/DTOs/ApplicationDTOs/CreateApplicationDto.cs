c305113017b7814453b2135bd1df1ae1156e30b5
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:27:04 +0300

    CreateApplicationDtoAdded

---
+++ CarSystem.API/Models/DTOs/ApplicationDTOs/CreateApplicationDto.cs
@@ -0,0 +1,18 @@
+﻿using CarSystem.API.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarSystem.API.Models.DTOs.ApplicationDTOs
+{
+    public class CreateApplicationDto
+    {
+        [Required(ErrorMessage = "User is required field!")]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Application type is required field")]
+        public ApplicationType ApplicationType { get; set; }
+
+
+        [Required(ErrorMessage = "License class type is required field!")]
+        public LicenseClassType LicenseClassType { get; set; }
+    }
+}
