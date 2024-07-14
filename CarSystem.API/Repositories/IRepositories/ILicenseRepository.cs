ca832daca8bd55255302a7164f59afece62b4ea5
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:04:06 +0300

    ILicenseRepositoryAdded

---
+++ CarSystem.API/Repositories/IRepositories/ILicenseRepository.cs
@@ -0,0 +1,9 @@
+﻿using CarSystem.API.Repositories.Base;
+using CarSystem.API.Models.Domain;
+
+namespace CarSystem.API.Repositories.IRepositories
+{
+    public interface ILicenseRepository : IEntityBaseRepository<License>
+    {
+    }
+}
