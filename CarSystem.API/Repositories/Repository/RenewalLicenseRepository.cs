62db9c47c3ac0fcf4f024575dd80eed6ad16bc5c
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:19:58 +0300

    RenewalLicenseRepositoryAdded

---
+++ CarSystem.API/Repositories/Repository/RenewalLicenseRepository.cs
@@ -0,0 +1,17 @@
+﻿using CarSystem.API.Data;
+using CarSystem.API.Models.Domain;
+using CarSystem.API.Repositories.Base;
+using CarSystem.API.Repositories.IRepositories;
+
+namespace CarSystem.API.Repositories.Repository
+{
+    public class RenewalLicenseRepository : EntityBaseRepository<RenewalLicense>, IRenewalLicenseRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RenewalLicenseRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
