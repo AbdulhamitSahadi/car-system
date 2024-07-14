5fd42727fd3eaccf55f70b31406e8d45605ae881
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 13:48:46 +0300

    UserDomainModelAdded

---
+++ CarSystem.API/Models/Domain/User.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarSystem.API.Models.Domain
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        [Required]
+        [ForeignKey("Person")]
+        public int PersonId { get; set; }
+        public Person Person { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        [ForeignKey("EditedBy")]
+        public int? AdminId { get; set; }
+        public Admin? EditedBy { get; set; }
+
+        public ICollection<Application> Applications { get; set; }
+    }
+}
