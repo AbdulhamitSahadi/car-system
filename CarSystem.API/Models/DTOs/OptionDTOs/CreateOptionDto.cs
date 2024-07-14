325394ec382212745bd8079d63c404d1a3f483d4
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:34:38 +0300

    CreateOptionDtoAdded

---
+++ CarSystem.API/Models/DTOs/OptionDTOs/CreateOptionDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSystem.API.Models.DTOs.OptionDTOs
+{
+    public class CreateOptionDto
+    {
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public bool IsCorrect { get; set; }
+
+        [Required]
+        public int QuestionId { get; set; }
+    }
+}
