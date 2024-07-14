05a895459036d90a0420147fc2d4c2c03bda0815
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:38:44 +0300

    UpdateTestCategoryDtoAdded

---
+++ CarSystem.API/Models/DTOs/TestCategoryDTOs/UpdateTestCategoryDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSystem.API.Models.DTOs.TestCategoryDTOs
+{
+    public class UpdateTestCategoryDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Fees { get; set; }
+
+        [Required]
+        public int NumberOfQuestions { get; set; }
+    }
+}
