55ca9aa8509d88fa5ab18fdef9d2a96bcf56d426
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:41:08 +0300

    WritingTestDtoAdded

---
+++ CarSystem.API/Models/DTOs/WritingTestDTOs/WritingTestDto.cs
@@ -0,0 +1,18 @@
+﻿using CarSystem.API.Models.Domain;
+
+namespace CarSystem.API.Models.DTOs.WritingTestDTOs
+{
+    public class WritingTestDto
+    {
+        public int Id { get; set; }
+        public decimal Score { get; set; }
+
+        public string? Notes { get; set; }
+
+
+        public bool IsSuccess { get; set; }
+
+
+        public Test Test { get; set; }
+    }
+}
