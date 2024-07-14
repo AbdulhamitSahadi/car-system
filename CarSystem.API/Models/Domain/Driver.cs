e47a3af3e1d9b2d91a37140ecc5c546e6524188f
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 13:45:08 +0300

    DriverDomainModelAdded

---
+++ CarSystem.API/Models/Domain/Driver.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarSystem.API.Models.Domain
+{
+    public class Driver
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        [Required]
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        [Required]  
+        [ForeignKey("EditedBy")]
+        public int AdminId { get; set; }
+        public Admin EditedBy { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+
+        public ICollection<License> OwnedLicenses { get; set; }
+    }
+}
