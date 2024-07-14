bc0d3b5270c96bee00e1d6a14d176f8fa14d24a3
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:34:29 +0300

    OptionDtoAdded

---
+++ CarSystem.API/Models/DTOs/OptionDTOs/OptionDto.cs
@@ -0,0 +1,15 @@
+﻿using CarSystem.API.Models.Domain;
+
+namespace CarSystem.API.Models.DTOs.OptionDTOs
+{
+    public class OptionDto
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public bool IsCorrect { get; set; }
+
+        public Question Question { get; set; }
+    }
+}
