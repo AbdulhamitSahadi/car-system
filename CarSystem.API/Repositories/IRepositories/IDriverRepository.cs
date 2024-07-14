3bf9d3b2010856d92465f5344abeebc1108abea1
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:03:27 +0300

    IDriverRepositoryAdded

---
+++ CarSystem.API/Repositories/IRepositories/IDriverRepository.cs
@@ -0,0 +1,10 @@
+﻿using CarSystem.API.Models.Domain;
+using CarSystem.API.Repositories.Base;
+
+namespace CarSystem.API.Repositories.IRepositories
+{
+    public interface IDriverRepository : IEntityBaseRepository<Driver>
+    {
+
+    }
+}
