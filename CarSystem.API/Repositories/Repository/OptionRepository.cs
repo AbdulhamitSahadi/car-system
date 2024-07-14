51c215bba07a8718e9e47cebde7247eb3ce0be78
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:19:18 +0300

    OptionRepositoryAdded

---
+++ CarSystem.API/Repositories/Repository/OptionRepository.cs
@@ -0,0 +1,17 @@
+﻿using CarSystem.API.Data;
+using CarSystem.API.Models.Domain;
+using CarSystem.API.Repositories.Base;
+using CarSystem.API.Repositories.IRepositories;
+
+namespace CarSystem.API.Repositories.Repository
+{
+    public class OptionRepository : EntityBaseRepository<Option>, IOptionRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OptionRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
