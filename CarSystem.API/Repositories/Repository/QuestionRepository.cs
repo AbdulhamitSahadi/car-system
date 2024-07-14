f8e9e75d15495bcba3f73a2bff4e8a1c1319e13c
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:19:29 +0300

    QuestionRepositoryAdded

---
+++ CarSystem.API/Repositories/Repository/QuestionRepository.cs
@@ -0,0 +1,17 @@
+﻿using CarSystem.API.Data;
+using CarSystem.API.Models.Domain;
+using CarSystem.API.Repositories.Base;
+using CarSystem.API.Repositories.IRepositories;
+
+namespace CarSystem.API.Repositories.Repository
+{
+    public class QuestionRepository : EntityBaseRepository<Question>, IQuestionRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuestionRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
