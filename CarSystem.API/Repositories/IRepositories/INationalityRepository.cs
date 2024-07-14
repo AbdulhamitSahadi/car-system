f229f10980de2a95e6694c77631287abdc1f119d
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:04:41 +0300

    INationalityRepositoryAdded

---
+++ CarSystem.API/Repositories/IRepositories/INationalityRepository.cs
@@ -0,0 +1,9 @@
+﻿using CarSystem.API.Models.Domain;
+using CarSystem.API.Repositories.Base;
+
+namespace CarSystem.API.Repositories.IRepositories
+{
+    public interface INationalityRepository : IEntityBaseRepository<Nationality>
+    {
+    }
+}
