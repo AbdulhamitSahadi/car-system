3eafc83c4ddd552face95ab4ac3bad97003c15e5
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 13:49:01 +0300

    VisionTestDomainModelAdded

---
+++ CarSystem.API/Models/Domain/VisionTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarSystem.API.Models.Domain
+{
+    public class VisionTest
+    {
+        [Key]
+        public int Id { get; set; }
+
+        //[Required]
+        //[ForeignKey(nameof(Application))]
+        //public int ApplicationId { get; set; }
+        //public Application Application { get; set; }
+
+        [DisplayFormat(DataFormatString = "0:C2", ApplyFormatInEditMode = true)]
+        public decimal Score { get; set; }
+
+        [StringLength(500)]
+        public string? Notes { get; set; }
+
+        [Required]
+        public bool IsSuccess { get; set; }
+
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        [Required]
+        [ForeignKey("EditedBy")]
+        public int AdminId { get; set; }
+        public Admin EditedBy { get; set; }
+
+        [Required]
+        [ForeignKey("Test")]
+        public int TestId { get; set; }
+        public Test Test { get; set; }
+    }
+}
