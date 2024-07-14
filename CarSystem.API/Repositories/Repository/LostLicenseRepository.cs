9e88e2363ef13f4d3d760c469ae959d744c5d7c5
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:17:41 +0300

    LostLicenseRepositoryAdded

---
+++ CarSystem.API/Repositories/Repository/LostLicenseRepository.cs
@@ -0,0 +1,17 @@
+﻿using CarSystem.API.Data;
+using CarSystem.API.Models.Domain;
+using CarSystem.API.Repositories.Base;
+using CarSystem.API.Repositories.IRepositories;
+
+namespace CarSystem.API.Repositories.Repository
+{
+    public class LostLicenseRepository : EntityBaseRepository<LostLicense>, ILostLicenseRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LostLicenseRepository(ApplicationDbContext context) : base(context) 
+        {
+            _context = context;
+        }
+    }
+}
