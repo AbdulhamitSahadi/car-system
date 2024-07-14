97db105b1dc577d8aaeaec1159673002f7e45c36
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:20:20 +0300

    TestRepositoryAdded

---
+++ CarSystem.API/Repositories/Repository/TestRepository.cs
@@ -0,0 +1,17 @@
+﻿using CarSystem.API.Data;
+using CarSystem.API.Models.Domain;
+using CarSystem.API.Repositories.Base;
+using CarSystem.API.Repositories.IRepositories;
+
+namespace CarSystem.API.Repositories.Repository
+{
+    public class TestRepository : EntityBaseRepository<Test>, ITestRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TestRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
