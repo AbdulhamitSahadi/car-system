2eefbe0d25ee97bae9d20c88293fc7642d034924
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:32:41 +0300

    CreateLostLicenseDtoAdded

---
+++ CarSystem.API/Models/DTOs/LostLicenseDTOs/CreateLostLicenseDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSystem.API.Models.DTOs.LostLicenseDTOs
+{
+    public class CreateLostLicenseDto
+    {
+        [Required]
+        public DateTime DateReported { get; set; }
+
+        [Required]
+        public string Reason { get; set; }
+
+        public string? Notes { get; set; }
+
+        public int LicenseId { get; set; }
+
+        public int ApplicationId { get; set; }
+    }
+}
