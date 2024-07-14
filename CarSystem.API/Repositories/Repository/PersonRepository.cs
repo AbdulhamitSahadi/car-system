b910d54cc600948d4a3666d562d5ebd03a8ba67b
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:19:00 +0300

    PersonRepositoryAdded

---
+++ CarSystem.API/Repositories/Repository/PersonRepository.cs
@@ -0,0 +1,17 @@
+﻿using CarSystem.API.Data;
+using CarSystem.API.Models.Domain;
+using CarSystem.API.Repositories.Base;
+using CarSystem.API.Repositories.IRepositories;
+
+namespace CarSystem.API.Repositories.Repository
+{
+    public class PersonRepository : EntityBaseRepository<Person>, IPersonRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PersonRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
