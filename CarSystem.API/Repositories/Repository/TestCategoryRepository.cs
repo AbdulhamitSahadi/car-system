edf7a644985718138d7ae5edc05d004e2430b7af
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:20:11 +0300

    TestCategoryRepositoryAdded

---
+++ CarSystem.API/Repositories/Repository/TestCategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using CarSystem.API.Data;
+using CarSystem.API.Models.Domain;
+using CarSystem.API.Repositories.Base;
+using CarSystem.API.Repositories.IRepositories;
+
+namespace CarSystem.API.Repositories.Repository
+{
+    public class TestCategoryRepository : EntityBaseRepository<TestCategory>, ITestCategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TestCategoryRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
