e51f6fd9dba9a98b2033c605ab3d0eeb7f0c88a8
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:16:56 +0300

    DamageLicenseRepositoryAdded

---
+++ CarSystem.API/Repositories/Repository/DamageLicenseRepository.cs
@@ -0,0 +1,17 @@
+﻿using CarSystem.API.Data;
+using CarSystem.API.Models.Domain;
+using CarSystem.API.Repositories.Base;
+using CarSystem.API.Repositories.IRepositories;
+
+namespace CarSystem.API.Repositories.Repository
+{
+    public class DamageLicenseRepository : EntityBaseRepository<DamageLicense>, IDamageLicenseRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DamageLicenseRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
