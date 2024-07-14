b6c7072493c6700ecf6f0b2027c87d8337291df3
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:28:37 +0300

    UpdateDriverDtoAdded

---
+++ CarSystem.API/Models/DTOs/DriverDTOs/UpdateDriverDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSystem.API.Models.DTOs.DriverDTOs
+{
+    public class UpdateDriverDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+    }
+}
