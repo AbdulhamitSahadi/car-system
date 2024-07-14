4b81d0e8b92ff30443297df87c62e83d02e465d5
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:34:58 +0300

    PersonDtoAdded

---
+++ CarSystem.API/Models/DTOs/PersonDTOs/PersonDto.cs
@@ -0,0 +1,44 @@
+﻿using CarSystem.API.Enums;
+using CarSystem.API.Models.Domain;
+
+namespace CarSystem.API.Models.DTOs.PersonDTOs
+{
+    public class PersonDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
+        public string Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+
+        public Nationality Nationality { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        //[NotMapped] 
+        public int Age
+        {
+            get
+            {
+                DateTime now = DateTime.UtcNow;
+                int age = now.Year - BirthDate.Year;
+
+                if (BirthDate.Date > now.AddYears(-age))
+                {
+                    age--;
+                }
+
+                return age;
+            }
+        }
+    }
+}
