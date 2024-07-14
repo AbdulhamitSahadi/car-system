fc2c51201bde5d2ed632fd706eff4ab633638636
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:20:39 +0300

    VisionTestRepositoryAdded

---
+++ CarSystem.API/Repositories/Repository/VisionTestRepository.cs
@@ -0,0 +1,17 @@
+﻿using CarSystem.API.Data;
+using CarSystem.API.Models.Domain;
+using CarSystem.API.Repositories.Base;
+using CarSystem.API.Repositories.IRepositories;
+
+namespace CarSystem.API.Repositories.Repository
+{
+    public class VisionTestRepository : EntityBaseRepository<VisionTest>, IVisionTestRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VisionTestRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
