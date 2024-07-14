440e6872736c0d2fff6ac17a650b0510f22e9e7b
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:37:29 +0300

    UpdateRenewalLicenseDtoAdded

---
+++ CarSystem.API/Models/DTOs/RenewalLicenseDTOs/UpdateRenewalLicenseDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSystem.API.Models.DTOs.RenewalLicenseDTOs
+{
+    public class UpdateRenewalLicenseDto
+    {
+        public int Id { get; set; }
+
+        //[Required]
+        //public int LicenseId { get; set; }
+
+        //[Required]
+        //public int ApplicationId { get; set; }
+
+        public DateTime RenewalDate { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public string? Notes { get; set; }
+    }
+}
