ce383dea7835f759cd6be9d10d2e3494d4f6478f
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:37:01 +0300

    RenewalLicenseDtoAdded

---
+++ CarSystem.API/Models/DTOs/RenewalLicenseDTOs/RenewalLicenseDto.cs
@@ -0,0 +1,19 @@
+﻿using CarSystem.API.Models.Domain;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarSystem.API.Models.DTOs.RenewalLicenseDTOs
+{
+    public class RenewalLicenseDto
+    {
+        public int Id { get; set; }
+        public License License { get; set; }
+
+        public Application Application { get; set; }
+
+        public DateTime RenewalDate { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+        public string? Notes { get; set; }
+    }
+}
