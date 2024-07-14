2d0b8f24e0be73bc12a6a64db5c2e21c4c7773a8
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:17:58 +0300

    NationalityRepositoryAdded

---
+++ CarSystem.API/Repositories/Repository/NationalityRepository.cs
@@ -0,0 +1,22 @@
+﻿using CarSystem.API.Data;
+using CarSystem.API.Models.Domain;
+using CarSystem.API.Repositories.Base;
+using CarSystem.API.Repositories.IRepositories;
+
+namespace CarSystem.API.Repositories.Reposiltory
+{
+    public class NationalityRepository : EntityBaseRepository<Nationality> , INationalityRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NationalityRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        //public override Task<bool> UpdateAsync(Nationality entity)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
