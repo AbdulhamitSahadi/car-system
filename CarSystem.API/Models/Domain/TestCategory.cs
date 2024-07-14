1002e78c43c1ff669da736c126edd984aacfa01e
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 13:48:31 +0300

    TestCategoryDomainModelAdded

---
+++ CarSystem.API/Models/Domain/TestCategory.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSystem.API.Models.Domain
+{
+    public class TestCategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public double Fees { get; set; }
+
+        [Required]
+        [Range(10, 100)]
+
+        public int NumberOfQuestions { get; set; }
+    }
+}
