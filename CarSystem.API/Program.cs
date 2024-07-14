5fb3cd5b20a025c516d1c940bcf28db2f19f19b7
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:46:22 +0300

    RegisteringRepositoriesToDIContainer

--- CarSystem.API/Program.cs
+++ CarSystem.API/Program.cs
@@ -1,5 +1,8 @@
 using CarSystem.API.Data;
 using CarSystem.API.Models.Mappers;
+using CarSystem.API.Repositories.IRepositories;
+using CarSystem.API.Repositories.Reposiltory;
+using CarSystem.API.Repositories.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +23,27 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 //Registering AutoMapper
 builder.Services.AddAutoMapper(typeof(MappingConfig));
+
+
+//Registering Repositories to DI container
+builder.Services.AddScoped<IAdminRepository, AdminRepository>();
+builder.Services.AddScoped<IApplicationRepository, ApplicationRepository>();
+builder.Services.AddScoped<IDamageLicenseRepository, DamageLicenseRepository>();
+builder.Services.AddScoped<IDriverRepository, DriverRepository>();
+builder.Services.AddScoped<ILicenseRepository, LicenseRepository>();
+builder.Services.AddScoped<ILicenseClassRepository, LicenseClassRepository>();
+builder.Services.AddScoped<ILostLicenseRepository, LostLicenseRepository>();
+builder.Services.AddScoped<INationalityRepository, NationalityRepository>();
+builder.Services.AddScoped<IOptionRepository, OptionRepository>();
+builder.Services.AddScoped<IPersonRepository, PersonRepository>();
+builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
+builder.Services.AddScoped<IRenewalLicenseRepository, RenewalLicenseRepository>();
+builder.Services.AddScoped<ITestCategoryRepository, TestCategoryRepository>();
+builder.Services.AddScoped<ITestRepository, TestRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IVisionTestRepository, VisionTestRepository>();
+builder.Services.AddScoped<IWritingTestRepository, WritingTestRepository>();
+
 
 var app = builder.Build();
 
