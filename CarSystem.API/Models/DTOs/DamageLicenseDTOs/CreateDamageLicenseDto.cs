75f8655409759b071f3782deded7bde7e4e9bcb9
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:27:50 +0300

    CreateDamageLicenseDtoAdded

---
+++ CarSystem.API/Models/DTOs/DamageLicenseDTOs/CreateDamageLicenseDto.cs
@@ -0,0 +1,24 @@
+﻿using CarSystem.API.Models.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarSystem.API.Models.DTOs.DamageLicenseDTOs
+{
+    //not for user, it's an admin role
+    public class CreateDamageLicenseDto
+    {
+        [Required]
+        public int LicenseId { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Reason { get; set; }
+
+        [Required]
+        public int DateReported { get; set; }
+
+        public string? Notes { get; set; }
+
+        [Required]
+        public Application ApplicationId { get; set; }
+    }
+}
