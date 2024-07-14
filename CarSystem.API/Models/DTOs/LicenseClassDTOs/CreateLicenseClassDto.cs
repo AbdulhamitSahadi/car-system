f552e7d942d0ba7ee0ba16f2001032028baccf78
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:31:23 +0300

    CreateLicenseClassDtoAdded

---
+++ CarSystem.API/Models/DTOs/LicenseClassDTOs/CreateLicenseClassDto.cs
@@ -0,0 +1,29 @@
+﻿using CarSystem.API.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarSystem.API.Models.DTOs.LicenseClassDTOs
+{
+    public class CreateLicenseClassDto
+    {
+        [Required]
+        public LicenseClassType Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required]
+        [Range(18, 80)]
+        public int MinimumAllowedAge { get; set; }
+
+
+        [Required]
+        [Range(1, 10)]
+        public int ValidityLength { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public double Fees { get; set; }
+
+
+        public string? Notes { get; set; }
+    }
+}
