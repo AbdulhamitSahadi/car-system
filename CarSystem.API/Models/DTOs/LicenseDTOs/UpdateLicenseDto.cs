a229ee1a72c85170bb22fc510f07f5f808ef91e4
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:32:21 +0300

    UpdateLicenseDtoAdded

---
+++ CarSystem.API/Models/DTOs/LicenseDTOs/UpdateLicenseDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSystem.API.Models.DTOs.LicenseDTOs
+{
+    public class UpdateLicenseDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+    }
+}
