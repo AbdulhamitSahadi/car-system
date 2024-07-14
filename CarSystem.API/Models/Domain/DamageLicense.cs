ec69b525f2be4a22a41f117c4bbfc1968bed7d6e
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 13:44:23 +0300

    DamageLicenseDomainModelAdded

---
+++ CarSystem.API/Models/Domain/DamageLicense.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Contracts;
+
+namespace CarSystem.API.Models.Domain
+{
+    public class DamageLicense
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(License))]
+        public int LicenseId { get; set; }
+        public License License { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime DateReported { get; set; }
+
+        [StringLength(500)]
+        public string? Notes { get; set; }
+
+        [Required]
+        [ForeignKey("Application")]
+        public int ApplicationId { get; set; }
+        public Application Application { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Reason { get; set; }
+
+        [Required]
+        [ForeignKey("EditedBy")]
+        public int AdminId { get; set; }
+        public Admin EditedBy { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
