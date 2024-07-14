3edb1b38b4f84fe5fc12f5dcf217c31260200170
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:26:54 +0300

    ApplicationDtoAdded

---
+++ CarSystem.API/Models/DTOs/ApplicationDTOs/ApplicationDto.cs
@@ -0,0 +1,18 @@
+﻿using CarSystem.API.Enums;
+using CarSystem.API.Models.Domain;
+
+namespace CarSystem.API.Models.DTOs.ApplicationDTOs
+{
+    public class ApplicationDto
+    {
+        public int Id { get; set; }
+
+        public User User { get; set; }
+
+        public ApplicationStatus Status { get; set; }
+        public ApplicationType Type { get; set; }
+
+        public LicenseClassType LicenseClassType { get; set; }
+
+    }
+}
