3f596b85dfe85f939968db1d18d7f88d6d17bbaf
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:33:50 +0300

    CreateNationalityDtoAdded

---
+++ CarSystem.API/Models/DTOs/NationalityDTOs/CreateNationalityDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSystem.API.Models.DTOs.NationalityDTOs
+{
+    public class CreateNationalityDto
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
