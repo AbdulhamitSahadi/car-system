828623fcb64e3cc4d29472b3912e7bfb8fd7afe2
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:28:02 +0300

    UpdateDamageLicenseDtoAdded

---
+++ CarSystem.API/Models/DTOs/DamageLicenseDTOs/UpdateDamageLicenseDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSystem.API.Models.DTOs.DamageLicenseDTOs
+{
+    public class UpdateDamageLicenseDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Reason { get; set; }
+    }
+}
