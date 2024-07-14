04e063e89f87bdb6b573eaca9b472e18da7491d4
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:27:14 +0300

    UpdateApplicationDtoAdded

---
+++ CarSystem.API/Models/DTOs/ApplicationDTOs/UpdateApplicationDto.cs
@@ -0,0 +1,11 @@
+﻿using CarSystem.API.Enums;
+
+namespace CarSystem.API.Models.DTOs.ApplicationDTOs
+{
+    public class UpdateApplicationDto
+    {
+        public int Id { get; set; }
+
+        public ApplicationType ApplicationType { get; set; }
+    }
+}
