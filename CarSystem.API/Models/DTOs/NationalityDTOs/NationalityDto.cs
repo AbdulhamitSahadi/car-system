c77d4da24e822b8cc7fdc2c41b36cdf4ee46674a
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:33:33 +0300

    NationalityDtoAdded

---
+++ CarSystem.API/Models/DTOs/NationalityDTOs/NationalityDto.cs
@@ -0,0 +1,13 @@
+﻿using CarSystem.API.Models.Domain;
+
+namespace CarSystem.API.Models.DTOs.NationalityDTOs
+{
+    public class NationalityDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Person> People { get; set; }
+    }
+}
