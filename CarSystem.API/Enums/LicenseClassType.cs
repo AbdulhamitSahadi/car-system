68d9e7ef33e5f1ab77f237b96e31bf5f7e42f228
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 13:51:10 +0300

    LicenseClassTypeEnumAdded

---
+++ CarSystem.API/Enums/LicenseClassType.cs
@@ -0,0 +1,18 @@
+﻿namespace CarSystem.API.Enums
+{
+    public enum LicenseClassType
+    {
+        ClassA,
+        ClassB,
+        ClassC,
+        ClassD,
+        ClassM,
+        ClassP,
+        ClassE,
+        ClassF,
+        ClassG,
+        ClassH,
+        ClassI,
+        ClassJ
+    }
+}
