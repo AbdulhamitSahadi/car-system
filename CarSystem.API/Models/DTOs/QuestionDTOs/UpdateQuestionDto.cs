b044f886fff6c38a6fe3c83734cd735c9063bdee
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-14 14:36:40 +0300

    UpdateQuestionDtoAdded

---
+++ CarSystem.API/Models/DTOs/QuestionDTOs/UpdateQuestionDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSystem.API.Models.DTOs.QuestionDTOs
+{
+    public class UpdateQuestionDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+
+        [Required]
+        public int TestCategoryId { get; set; }
+    }
+}
