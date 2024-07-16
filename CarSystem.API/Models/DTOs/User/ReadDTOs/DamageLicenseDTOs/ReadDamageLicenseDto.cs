709254c795532eac08d34a0201cec64e0a2e5970
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-16 18:00:02 +0300

    ReadUserDTOs

---
+++ CarSystem.API/Models/DTOs/User/ReadDTOs/DamageLicenseDTOs/ReadDamageLicenseDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using CarSystem.API.Models.Domain;
+
+namespace CarSystem.API.Models.DTOs.User.ReadDTOs.DamageLicenseDTOs
+{
+    public class ReadDamageLicenseDto
+    {
+        public int Id { get; set; }
+
+        //[Required]
+        //[ForeignKey(nameof(License))]
+        public License License { get; set; }
+
+        //[Required]
+        //[DataType(DataType.DateTime)]
+        public DateTime DateReported { get; set; }
+
+        //[StringLength(500)]
+        public string? Notes { get; set; }
+
+        //[Required]
+        //[ForeignKey("Application")]
+        public Application Application { get; set; }
+
+        //[Required]
+        //[StringLength(200)]
+        public string Reason { get; set; }
+    }
+}
---
+++ CarSystem.API/Models/DTOs/User/ReadDTOs/UserDTOs/ReadUserDto.cs
@@ -0,0 +1,22 @@
+﻿using CarSystem.API.Models.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarSystem.API.Models.DTOs.User.ReadDTOs.UserDTOs
+{
+    public class ReadUserPermissionDto
+    {
+        public int Id { get; set; }
+
+
+        public string UserName { get; set; }
+
+
+        public bool IsActive { get; set; }
+
+
+        public int PersonId { get; set; }
+
+
+        public ICollection<Application> Applications { get; set; }
+    }
+}
