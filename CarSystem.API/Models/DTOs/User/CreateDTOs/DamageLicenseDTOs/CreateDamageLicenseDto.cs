91b5f228e5858eb65378bb9a198f20ec54bb4685
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-16 17:59:23 +0300

    CreateUserDTOs

---
+++ CarSystem.API/Models/DTOs/User/CreateDTOs/DamageLicenseDTOs/CreateDamageLicenseDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSystem.API.Models.DTOs.User.CreateDTOs.DamageLicenseDTOs
+{
+    public class CreateDamageLicenseDto
+    {
+        [StringLength(50)]
+        [Required(ErrorMessage = "Reason is required field!")]
+        public string Reason { get; set; }
+
+
+        [StringLength(500)]
+        public string? Notes { get; set; }
+    }
+}
