b8597cb9be495597fdc483d8c879ad05e8a2f27f
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-07-17 21:39:04 +0300

    Enums&ExtensionsUpdated

--- CarSystem.API/Enums/TestType.cs
+++ CarSystem.API/Enums/TestType.cs
@@ -3,6 +3,6 @@
     public enum TestType
     {
         Vision,
-        Written
+        Writing
     }
 }
--- CarSystem.API/Extensions/EnumConstantsToString.cs
+++ CarSystem.API/Extensions/EnumConstantsToString.cs
@@ -1,4 +1,6 @@
 ﻿using CarSystem.API.Enums;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
 
 namespace CarSystem.API.Extensions
 {
@@ -50,9 +52,9 @@ namespace CarSystem.API.Extensions
             return string.Empty;
         }
 
-        public static string ApplicationStatusToString(this Status applicationStatus)
+        public static string StatusToString(this Status status)
         {
-            switch (applicationStatus)
+            switch (status)
             {
                 case Status.Approved:
                     return "Approved";
@@ -148,6 +150,154 @@ namespace CarSystem.API.Extensions
                     return "Vision";
                 case QuestionType.Writing:
                     return "Writing";
+            }
+
+            return string.Empty;
+        }
+
+        public static string PermissionTypeToString(this PermissionType permissionType)
+        {
+            switch (permissionType)
+            {
+                case PermissionType.Critical:
+                    return "Critical";
+                case PermissionType.Uncritical:
+                    return "UnCritical";
+            }
+
+            return string.Empty;
+        }
+
+        public static string PermissionNameToString(this PermissionName permissionName)
+        {
+            switch(permissionName)
+            {
+                case PermissionName.CreateAdmin:
+                    return "Create Admin";
+                case PermissionName.CreateDriver:
+                    return "Create Driver";
+                case PermissionName.CreateLicenseClass:
+                    return "Create License Class";
+                case PermissionName.CreateLicense:
+                    return "Create License";
+                case PermissionName.CreateNationality:
+                    return "Create Nationality";
+                case PermissionName.CreateOption:
+                    return "Create Option";
+                case PermissionName.CreateQuestion:
+                    return "Create Question";
+                case PermissionName.CreateTestCategory:
+                    return "Create Test Category";
+                case PermissionName.CreateVisionTest:
+                    return "Create Vision Test";
+                case PermissionName.CreateWritingTest:
+                    return "Create Writing Test";
+                case PermissionName.ReadAdminAdmin:
+                    return "Read Admin Admin";
+                case PermissionName.ReadAdminApplication:
+                    return "Read Admin Application";
+                case PermissionName.ReadAdminDamageLicense:
+                    return "Read Admin Damage License";
+                case PermissionName.ReadAdminDriver:
+                    return "Read Admin Driver";
+                case PermissionName.ReadAdminLicenseClass:
+                    return "Read Admin License Class";
+                case PermissionName.ReadAdminLicense:
+                    return "Read Admin License";
+                case PermissionName.ReadAdminLostLicense:
+                    return "Read Admin Lost License";
+                case PermissionName.ReadAdminNationality:
+                    return "ReadAdminNationality";
+                case PermissionName.ReadAdminOption:
+                    return "Read Admin Option";
+                case PermissionName.ReadAdminPerson:
+                    return "Read Admin Person";
+                case PermissionName.ReadAdminQuestion:
+                    return "Read Admin Question";
+                case PermissionName.ReadAdminRenewalLicense:
+                    return "Read Admin Renewal License";
+                case PermissionName.ReadAdminTakeTest:
+                    return "Read Admin Take Test";
+                case PermissionName.ReadAdminTestCategory:
+                    return "Read Admin Test Category";
+                case PermissionName.ReadAdminTest:
+                    return "Read Admin Test";
+                case PermissionName.ReadAdminUser:
+                    return "Read Admin User";
+                case PermissionName.ReadAdminVisionTest:
+                    return "Read Admin Vision Test";
+                case PermissionName.ReadAdminWritingTest:
+                    return "Read Writing Test";
+                case PermissionName.ReadApplication:
+                    return "Read Application";
+                case PermissionName.ReadDamageLicense:
+                    return "Read Damage License";
+                case PermissionName.ReadDriver:
+                    return "Read Driver";
+                case PermissionName.ReadLicenseClass:
+                    return "Read License Class";
+                case PermissionName.ReadLicense:
+                    return "Read License";
+                case PermissionName.ReadLostLicense:
+                    return "Read Lost License";
+                case PermissionName.ReadNationality:
+                    return "Read Nationality";
+                case PermissionName.ReadOption:
+                    return "Read Option";
+                case PermissionName.ReadPerson:
+                    return "Read Person";
+                case PermissionName.ReadQuestion:
+                    return "Read Question";
+                case PermissionName.ReadRenewalLicense:
+                    return "Read Renewal License";
+                case PermissionName.ReadTakeTest:
+                    return "Read Take Test";
+                case PermissionName.ReadTestCategory:
+                    return "Read Test Category";
+                case PermissionName.ReadTest:
+                    return "Read Test";
+                case PermissionName.ReadUser:
+                    return "Read User";
+                case PermissionName.ReadVisionTest:
+                    return "Read Vision Test";
+                case PermissionName.ReadWritingTest:
+                    return "Read Writing Test";
+                case PermissionName.UpdateAdmin:
+                    return "UpdateAdmin";
+                case PermissionName.UpdateApplication:
+                    return "Update Application";
+                case PermissionName.UpdateDamageLicense:
+                    return "Update Damage License";
+                case PermissionName.UpdateDriver:
+                    return "Update Driver";
+                case PermissionName.UpdateLicenseClass:
+                    return "Update License Class";
+                case PermissionName.UpdateLicense:
+                    return "Update License";
+                case PermissionName.UpdateLostLicense:
+                    return "Update Lost License";
+                case PermissionName.UpdateNationality:
+                    return "Update Nationality";
+                case PermissionName.UpdateOption:
+                    return "Update Option";
+                case PermissionName.UpdatePerson:
+                    return "Update Person";
+                case PermissionName.UpdateQuestion:
+                    return "Update Question";
+                case PermissionName.UpdateRenewalLicense:
+                    return "Update Renewal License";
+                case PermissionName.UpdateTakeTest:
+                    return "Update Take Test";
+                case PermissionName.UpdateTestCategory:
+                    return "Update Test Category";
+                case PermissionName.UpdateTest:
+                    return "Update Test";
+                case PermissionName.UpdateUser:
+                    return "Update User";
+                case PermissionName.UpdateVisionTest:
+                    return "Update Vision Test";
+                case PermissionName.UpdateWritingTest:
+                    return "Update Writing Test";
             }
 
             return string.Empty;
